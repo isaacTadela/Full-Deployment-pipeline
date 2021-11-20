a62e112426c437e52c45fcd32d1ac6b0658ab965
Ubuntu <ubuntu@ip-172-31-39-14.eu-west-3.compute.internal>
2021-11-20 16:53:09 +0000

    found bag at vault 3.0.0, downgrade to 2.24.1

--- main.tf
+++ main.tf
@@ -8,47 +8,54 @@ terraform {
   }
 } 
 
+
 provider "vault" {
   # This will default use $VAULT_ADDR But can be set explicitly
   # address = "https://vault.example.net:8200"
   # I use $TF_VAR_VAULT_ADDR just for the practice
   address = var.VAULT_ADDR
   token = var.VAULT_TOKEN
+
+  # This version in here only because of a bug in version 3.0.0
+  # The issue link: https://github.com/hashicorp/terraform-provider-vault/issues/1226
+  # The issue is: policy_arns cannot be null on version 3.0.0
+  version = "~> 2.24.1"
 }
 
-resource "vault_aws_secret_backend" "aws-engine" {
+
+resource "vault_aws_secret_backend" "aws" {
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region = var.region
 
-  default_lease_ttl_seconds = "120"
-  max_lease_ttl_seconds     = "240"
+  # 10min
+  default_lease_ttl_seconds = "600" 
 }
 
 resource "vault_aws_secret_backend_role" "dev-admin" {
-  backend = "${vault_aws_secret_backend.aws-engine.path}"
+  backend = "${vault_aws_secret_backend.aws.path}"
   name    = "dev-admin-role"
   credential_type = "iam_user"
 
-  policy_document  = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Action": [
-          "iam:*", "ec2:*", "s3:*", "rds:*"
-        ],
-        "Resource": "*"
-      }
-    ]
-  }
-  EOF
+policy_document= <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "iam:*", "ec2:*", "rds:*", "elasticloadbalancing:*", "autoscaling:*", "cloudwatch:*"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 data "vault_aws_access_credentials" "creds" {
-  backend = "aws"
-  role    = "dev-admin-role"
+  backend = vault_aws_secret_backend.aws.path
+  role    = vault_aws_secret_backend_role.dev-admin.name
 }
 
 provider "aws" {
